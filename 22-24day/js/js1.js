67eb2ba6d2880e46d7bf07353fbc255deb989c4d
Code1157 <huangyuhua@hotmail.com>
2020-07-21 16:34:55 +0800

    22-24day

---
+++ 22-24day/js/js1.js
@@ -0,0 +1,48 @@
+/*
+实现一个字符串头尾去除空格的函数
+注意需要去除的空格，包括全角、半角空格
+暂时不需要学习和使用正则表达式的方式
+*/
+function diyTrim(str) {
+  var result = "";
+  result = str;
+  for (var i = 0; i < result.length; i++) {
+    result = result.replace(" ", "");
+    result = result.replace("　", "");
+  }
+  return result
+}
+
+// 测试用例
+console.log(diyTrim(' a f b    ')); // ->a f b
+console.log(diyTrim('    ffdaf    ')); // ->ffdaf
+console.log(diyTrim('1    ')); // ->1
+console.log(diyTrim('　　f')); // ->f
+console.log(diyTrim('  　  a f b 　　 ')); // ->a f b
+console.log(diyTrim(' ')); // ->
+console.log(diyTrim('　')); // ->
+console.log(diyTrim('')); // ->
+
+/*
+去掉字符串str中，连续重复的地方
+*/
+function removeRepetition(str) {
+  var result = "";
+  var temp = "";
+  var a = [];
+  a = str.split("");
+  // console.log(a);
+  for(var i=0; i<str.length; i++){
+    if(a.indexOf(a[i]) === i){
+      result += a[i];
+    }
+  }
+  return result;
+}
+
+// 测试用例
+console.log(removeRepetition("aaa")); // ->a
+console.log(removeRepetition("abbba")); // ->aba
+console.log(removeRepetition("aabbaabb")); // ->abab
+console.log(removeRepetition("")); // ->
+console.log(removeRepetition("abc")); // ->abc
